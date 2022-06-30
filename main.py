4fab4ddbb3216731c8613ef2c526bc9071f866e7
CaptLynx <epicsam75@outlook.com>
2022-06-30 11:51:26 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+input = input("What would you like to convert to morse code? Letters and numbers only: ").lower()
+
+morse_dict = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    " ": "/",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----."
+}
+
+morse_code = ""
+
+for letter in input:
+    if letter in morse_dict.keys():
+        morse_code += morse_dict[letter]
+        morse_code += " "
+
+print(morse_code)
